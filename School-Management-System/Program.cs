81fc1330f6c6d40adc466bb80580f4a8799a49a2
Asif <asifm279@gmail.com>
2023-03-23 12:17:16 -0700

    Add project files.

---
+++ School-Management-System/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Management_SystemOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Teacher mathteacher = new Teacher();
+            mathteacher.Name = "Hassan";
+            mathteacher.Birthday = 2-02-1990;
+            mathteacher.Speciality = "Math";
+
+
+            Teacher urduteacher = new Teacher();
+            urduteacher.Name = "Abdullah";
+            urduteacher.Birthday = 2 - 02 - 1991;
+            urduteacher.Speciality = "Urdu";
+
+
+            Teacher engteacher = new Teacher();
+            engteacher.Name = "Imran";
+            engteacher.Birthday = 2 - 02 - 1994;
+            engteacher.Speciality = "English";
+
+
+            Console.WriteLine(urduteacher.Name);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ School-Management-System/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Management_SystemOne
+{
+    class Teacher
+    {
+        public  string Name;
+        public int Birthday;
+        public string Speciality;
+    }
+}
